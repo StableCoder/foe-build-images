1285b505e1319b1a9aaefcdfd2f62c1f5b864912
George Cave <gcave@stablecoder.ca>
2025-04-03 17:45:06 -0400

    Update Windows install scripts for CMake 4.0.0
    CMake 4.0.0 removes support for CMake scripts pre 3.5, however some
    libraries still have scripts that old. For those, add an option to the
    CMake configuration to force the minimum to 3.5

--- windows-install-scripts/yaml-cpp.ps1
+++ windows-install-scripts/yaml-cpp.ps1
@@ -25,7 +25,7 @@ try {
 
     # Configure and Compile
     Write-Host "Configuring and compiling"
-    cmake -B build -G Ninja -D CMAKE_BUILD_TYPE="$BuildType" -D CMAKE_INSTALL_PREFIX="C:/yaml-cpp" -D YAML_BUILD_SHARED_LIBS=ON -D YAML_CPP_BUILD_CONTRIB=OFF -D YAML_CPP_BUILD_TESTS=OFF -D YAML_CPP_BUILD_TOOLS=OFF
+    cmake -B build -G Ninja -D CMAKE_BUILD_TYPE="$BuildType" -D CMAKE_INSTALL_PREFIX="C:/yaml-cpp" -D YAML_BUILD_SHARED_LIBS=ON -D YAML_CPP_BUILD_CONTRIB=OFF -D YAML_CPP_BUILD_TESTS=OFF -D YAML_CPP_BUILD_TOOLS=OFF -DCMAKE_POLICY_VERSION_MINIMUM="3.5"
     cmake --build build
     if($LastExitCode -ne 0) { throw }
 
